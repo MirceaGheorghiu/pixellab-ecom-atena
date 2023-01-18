78fd0aeb191fa796ad0bbe4389f372925f565c1c
MirceaGheorghiu <110449045+MirceaGheorghiu@users.noreply.github.com>
2023-01-18 21:59:43 +0200

    curs

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,3 +1,5 @@
-export default function App({ Component, pageProps }) {
+import './../styles/index.css';
+
+export default function App({Component, pageProps}) {
   return <Component {...pageProps} />;
 }
